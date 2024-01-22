4ef4a649e5915db7399f085bfefa294b9feb5214
ukemeikot <ukemeetim2222@gmail.com>
2024-01-22 12:34:26 +0100

    changes

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,20 +1,5 @@
 #include "sort.h"
-/**
-*el_swap - helps to swap elemets in an array
-*@array: the array to be worked on
-*@a: the first index
-*@b: the second index
-*Return: nothing
-*/
 
-void el_swap(int *array, size_t a, size_t b)
-{
-	int tmp;
-
-	tmp = array[a];
-	array[a] = array[b];
-	array[b] = tmp;
-}
 /**
   *selection_sort - sorts arrays using selection sort
   *@array: the array to be sorted
@@ -22,7 +7,7 @@ void el_swap(int *array, size_t a, size_t b)
   */
 void selection_sort(int *array, size_t size)
 {
-	size_t min, a, b;
+	size_t min, a, b, tmp;
 
 	if (size < 2)
 		return;
@@ -36,7 +21,9 @@ void selection_sort(int *array, size_t size)
 		}
 		if (min != a)
 		{
-			el_swap(array, a, min);
+			tmp = array[min];
+			array[min] = array[a];
+			array[a] = tmp;
 			print_array(array, size);
 		}
 	}
